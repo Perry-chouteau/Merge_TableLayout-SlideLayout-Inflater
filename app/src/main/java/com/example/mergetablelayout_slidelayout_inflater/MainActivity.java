4ed0d6f26d95ae0318a4df25c9189a3aca207bf9
perry chouteau <perry.chouteau@epitech.eu>
2021-09-08 15:15:35 +0200

    [ADD] : default empty activity

---
+++ app/src/main/java/com/example/mergetablelayout_slidelayout_inflater/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.mergetablelayout_slidelayout_inflater;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
